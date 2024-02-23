713b23e80e1af53bc342ca9e70a550e991a7a021
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-02-24 00:57:20 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+// 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program. 
+let programing_language:String[]=[
+    "Html",
+    "Css",
+    "Javascript",
+    "Typescript",
+    "python"
+]
+console.log(programing_language[5])
+console.log(programing_language[2])
