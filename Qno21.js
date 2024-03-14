103a969968fc734ffbb41a67040e0483409e132d
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:44:00 +0500

    Add files via upload

---
+++ Qno21.js
@@ -0,0 +1,5 @@
+"use strict";
+// Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let myDetails = { Name: "huzaifa", Father_name: "AbdulRab", Age: 17 };
+console.log(myDetails);
---
+++ Qno21.ts
@@ -0,0 +1,5 @@
+// Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let myDetails = {Name : "huzaifa" , Father_name : "AbdulRab", Age : 17}
+
+console.log(myDetails);
